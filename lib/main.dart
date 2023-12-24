a8d7715ae1ceec8da184bef7cad4bae48ba7a9b7
Parash <samirkhdk@hotmail.com>
2023-12-24 18:51:24 +0545

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:my_project/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
